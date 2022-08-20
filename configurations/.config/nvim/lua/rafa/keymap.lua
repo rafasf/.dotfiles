bd0512c8d2e3b1dd0fa17149c979a99e05a3a2ac
Rafael Ferreira <rafa@rafasf.com>
2022-08-20 18:02:24 -0500

    Move keybindings vim configuration to Lua
    type: vim

---
+++ configurations/.config/nvim/lua/rafa/keymap.lua
@@ -0,0 +1,22 @@
+local nnoremap = require("rafa.keybind").nnoremap
+local vnoremap = require("rafa.keybind").vnoremap
+
+vim.g.mapleader = ","
+
+nnoremap("<leader>y", "\"+y")
+vnoremap("<leader>y", "\"+y")
+
+
+nnoremap("<leader>n", ":set invhls<CR>:set hls?<CR>")
+
+nnoremap("<leader>h", ":wincmd h<CR>")
+nnoremap("<leader>j", ":wincmd j<CR>")
+nnoremap("<leader>k", ":wincmd k<CR>")
+nnoremap("<leader>l", ":wincmd l<CR>")
+
+nnoremap("^", ":setl hls<CR>:let @/=\"<C-r><C-w>\"<CR>")
+
+nnoremap("<leader>m", ":NvimTreeToggle<CR>")
+
+nnoremap("<leader>t", ":Files<CR>")
+nnoremap("<leader>b", ":Buffers<CR>")
