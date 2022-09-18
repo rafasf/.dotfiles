444a9b620a85e0166a1dc19065f75bf27125275f
Rafael Ferreira <rafa@rafasf.com>
2022-09-18 09:11:28 -0500

    Clean-up export variables
    type: sys

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -14,19 +14,8 @@ export CLICOLOR=1
 export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude .git";
 
 export XDG_CONFIG_HOME=${HOME}/.config
-#export XDG_DATA_HOME=${HOME}/.local
-
-# Golang
-export GOPATH=/usr/local/go
-export PATH=$PATH:$GOPATH/bin
 
 ## GPG Agent
 export GPG_TTY="$(tty)"
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-
-## Rust
-export PATH="$XDG_CONFIG_HOME/cargo/bin:$PATH"
-
-## Talisman
-export TALISMAN_HOME=${XDG_CONFIG_HOME}/talisman/bin
-
+gpgconf --launch gpg-agent
