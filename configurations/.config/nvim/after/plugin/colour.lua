9d079577b167795c9be436fb9dc7d6040f7964f8
Rafael Ferreira <rafa@rafasf.com>
2022-12-18 18:34:10 -0600

    Use Lua API for colour configuration
    type: vim

--- configurations/.config/nvim/after/plugin/colour.lua
+++
@@ -1,7 +0,0 @@
-vim.g.gruvbox_contrast_dark = "medium"
-vim.g.gruvbox_invert_selection = "0"
-
-vim.opt.background = "dark"
-
-vim.cmd("colorscheme gruvbox")
-vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
