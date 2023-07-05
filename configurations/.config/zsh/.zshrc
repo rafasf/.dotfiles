d7637895ad9f25ba8851328e4dc029b0a0cd41d7
Rafael Ferreira <rafa@rafasf.com>
2023-07-04 21:44:50 -0500

    Set variables for common applications to use XDG
    type: sys

--- configurations/.config/zsh/.zshrc
+++ configurations/.config/zsh/.zshrc
@@ -23,5 +23,4 @@ eval "$(starship init zsh)"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,10 +3,5 @@
 autoload -U compinit && compinit
 zmodload -i zsh/complist
 
-# Auto-complete from the project folder
-projets_root="$HOME/Projects"
-p() { cd $projets_root/$1; }
-compctl -/ -W $projets_root p
-
 # Fallback to built in ls colors
 zstyle ':completion:*' list-colors ''
