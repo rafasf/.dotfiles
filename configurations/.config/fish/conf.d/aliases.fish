666ee48ff68d558a294317519ce5a4755be6358f
Rafael Ferreira <rafa@rafasf.com>
2024-05-12 09:51:42 +0900

    Show directories together on ls by default
    type: sys

--- configurations/.config/fish/conf.d/aliases.fish
+++ configurations/.config/fish/conf.d/aliases.fish
@@ -10,7 +10,7 @@ alias cat "bat"
 alias gw "./gradlew"
 alias be "bundle exec"
 
-alias ls "eza --icons"
+alias ls "eza --icons --group-directories-first"
 alias ll "eza -l -g --icons"
 alias lla "eza -l -g -a --icons"
 alias tree "eza --tree"
