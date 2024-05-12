f3216de4d0aa2bc84a685a8a9f79e67716dd9b87
Rafael Ferreira <rafa@rafasf.com>
2024-05-12 10:23:14 +0900

    Add some more configs for apps to follow XDG
    type: sys

--- configurations/.config/fish/conf.d/config_dirs.fish
+++ configurations/.config/fish/conf.d/config_dirs.fish
@@ -5,6 +5,7 @@ set -gx XDG_CACHE_HOME $HOME/.cache
 
 # Many applications need explicit configuration to not put configuration in the
 # home folder
+set -gx NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
 set -gx CARGO_HOME $XDG_DATA_HOME/cargo
 set -gx DOCKER_CONFIG $XDG_CONFIG_HOME/docker
 set -gx GNUPGHOME $XDG_DATA_HOME/gnupg
@@ -12,6 +13,8 @@ set -gx GOPATH $XDG_DATA_HOME/go
 set -gx GRADLE_USER_HOME $XDG_DATA_HOME/gradle
 set -gx LESSHISTFILE $XDG_CACHE_HOME/less/history
 set -gx VOLTA_HOME $XDG_DATA_HOME/volta
+set -gx ANDROID_HOME $XDG_DATA_HOME/android
+set -gx HISTFILE $XDG_STATE_HOME/bash/history
 
 # Create basic configuration variables
 set -gx DOTS $HOME/.dotfiles
