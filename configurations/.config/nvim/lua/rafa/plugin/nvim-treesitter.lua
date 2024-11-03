b8f1d035616ad49eab99f91bc0519f9b60d42383
Rafael Ferreira <rafa@rafasf.com>
2024-11-03 10:06:53 +0100

    Tweak some keymappings and telescope find_files
    type: vim

--- configurations/.config/nvim/lua/rafa/plugin/nvim-treesitter.lua
+++ configurations/.config/nvim/lua/rafa/plugin/nvim-treesitter.lua
@@ -12,7 +12,7 @@ return {
       treesitter.setup({
         highlight = {
           enable = true,
-          additional_vim_regex_highlighting = false,
+          additional_vim_regex_highlighting = true,
         },
         indent = { enable = true },
         autotag = {
