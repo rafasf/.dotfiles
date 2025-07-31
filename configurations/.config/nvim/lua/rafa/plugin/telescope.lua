d1c30d1c322f914fe6532d3aa7e2b916aadf46de
Rafael Ferreira <rafa@rafasf.com>
2025-07-31 22:28:51 +0200

    Tidy-up telescope
    type: vim

--- configurations/.config/nvim/lua/rafa/plugin/telescope.lua
+++ configurations/.config/nvim/lua/rafa/plugin/telescope.lua
@@ -4,7 +4,7 @@ return {
   "nvim-telescope/telescope.nvim",
   branch = "0.1.x",
   dependencies = {
-    "nvim-lua/plenary.nvim",
+    { "nvim-lua/plenary.nvim" },
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     { "nvim-telescope/telescope-ui-select.nvim" },
     "nvim-tree/nvim-web-devicons",
@@ -14,7 +14,7 @@ return {
 
     telescope.setup({
       defaults = {
-        path_display = { "smart" },
+        path_display = { "shorten" },
       },
       pickers = {
         find_files = {
