612ae4d0307209f2e9010bcf17614120f44a0cab
Rafael Ferreira <rafa@rafasf.com>
2025-07-31 22:25:31 +0200

    Use Neotree
    Slightly cleaner look.
    
    type: vim

---
+++ configurations/.config/nvim/lua/rafa/plugin/neo-tree.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      { "\\", ":Neotree reveal<cr>", desc = "NeoTree reveal", silent = true },
+    },
+    opts = {
+      filesystem = {
+        window = {
+          mappings = {
+            ["\\"] = "close_window",
+          },
+        },
+      },
+    },
+  },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-neo-tree/neo-tree.nvim",
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
+}
