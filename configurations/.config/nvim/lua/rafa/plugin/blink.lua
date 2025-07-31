1aecc844a6d051f0fd92e2d1d090f6d4260bb62a
Rafael Ferreira <rafa@rafasf.com>
2025-07-31 22:27:28 +0200

    Tidy-up blink
    type: vim

--- configurations/.config/nvim/lua/rafa/plugin/blink.lua
+++ configurations/.config/nvim/lua/rafa/plugin/blink.lua
@@ -1,17 +1,44 @@
 return {
   {
     "saghen/blink.cmp",
-    dependencies = "rafamadriz/friendly-snippets",
+    dependencies = {
+      -- Snippet Engine
+      {
+        "L3MON4D3/LuaSnip",
+        version = "2.*",
+        build = (function()
+          return "make install_jsregexp"
+        end)(),
+        dependencies = {
+          {
+            "rafamadriz/friendly-snippets",
+            config = function()
+              require("luasnip.loaders.from_vscode").lazy_load()
+            end,
+          },
+        },
+        opts = {},
+      },
+      "folke/lazydev.nvim",
+    },
 
-    version = "v0.*",
+    version = "v1.*",
     opts = {
       keymap = { preset = "default" },
 
       appearance = {
-        use_nvim_cmp_as_default = true,
         nerd_font_variant = "mono",
       },
 
+      sources = {
+        default = { "lsp", "path", "snippets", "lazydev" },
+        providers = {
+          lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+        },
+      },
+
+      snippets = { preset = "luasnip" },
+      fuzzy = { implementation = "lua" },
       signature = { enabled = true },
     },
     opts_extend = { "sources.default" },
