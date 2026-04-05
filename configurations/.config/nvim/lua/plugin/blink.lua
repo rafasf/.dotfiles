8393b8c1e8a100a534952c65dd435541a28e3ba2
Rafael Ferreira <rafa@rafasf.com>
2026-04-05 16:43:27 +0200

    Update vim config

--- configurations/.config/nvim/lua/plugin/blink.lua
+++ configurations/.config/nvim/lua/plugin/blink.lua
@@ -12,7 +12,7 @@ return {
       },
 
       sources = {
-        default = { "lsp", "path", "snippets", "lazydev" },
+        default = { "lsp", "path", "snippets", "buffer", "lazydev" },
         providers = {
           lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
         },
